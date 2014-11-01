a5502261647ab54186e2d8ca52cf98934296893b
Jeremy Kahn <jeremyckahn@gmail.com>
2014-11-01 16:12:40 -0500

    First-pass YUIDoc conversion for rekapi.keyframe-property.js.

--- src/rekapi.keyframe-property.js
+++ src/rekapi.keyframe-property.js
@@ -9,11 +9,18 @@ rekapiModules.push(function (context) {
   var interpolate = Tweenable.interpolate;
 
   /**
-   * Represents an individual component of an actor's keyframe state.  In most cases you won't need to deal with this object directly, as the [`Rekapi.Actor`](rekapi.actor.js.html#Actor) APIs abstract a lot of what this Object does away for you.
-   * @param {number} millisecond Where on the animation timeline this KeyframeProperty is.
+   * Represents an individual component of an actor's keyframe state.  In most
+   * cases you won't need to deal with this object directly, as the
+   * [`Rekapi.Actor`](rekapi.actor.js.html#Actor) APIs abstract a lot of what
+   * this Object does away for you.
+   * @class Rekapi.KeyframeProperty
+   * @param {number} millisecond Where on the animation timeline this
+   * KeyframeProperty is.
    * @param {string} name The property's name, such as "x" or "opacity."
-   * @param {number|string|Function} value The value that this KeyframeProperty represents.
-   * @param {string=} opt_easing The easing curve at which this KeyframeProperty should be animated to.  Defaults to "linear".
+   * @param {number|string|Function} value The value that this KeyframeProperty
+   * represents.
+   * @param {string=} opt_easing The easing curve at which this
+   * KeyframeProperty should be animated to.  Defaults to "linear".
    * @constructor
    */
   Rekapi.KeyframeProperty = function (millisecond, name, value, opt_easing) {
@@ -30,12 +37,16 @@ rekapiModules.push(function (context) {
   var KeyframeProperty = Rekapi.KeyframeProperty;
 
   /**
-   * Modify a [`Rekapi.KeyframeProperty`](#KeyframeProperty).  Any of the following are valid properties of `newProperties` and correspond to the parameters of the [`Rekapi.KeyframeProperty`](#KeyframeProperty) constructor:
+   * Modify a [`Rekapi.KeyframeProperty`](#KeyframeProperty).  Any of the
+   * following are valid properties of `newProperties` and correspond to the
+   * parameters of the [`Rekapi.KeyframeProperty`](#KeyframeProperty)
+   * constructor:
    *
    * - _millisecond_ (__number__)
    * - _name_ (__string__)
    * - _value_ (__number|string__)
    * - _easing_ (__string__)
+   * @method modifyWith
    * @param {Object} newProperties
    */
   KeyframeProperty.prototype.modifyWith = function (newProperties) {
@@ -50,10 +61,19 @@ rekapiModules.push(function (context) {
   };
 
   /**
-   * Calculate the midpoint between this [`Rekapi.KeyframeProperty`](#KeyframeProperty) and the next [`Rekapi.KeyframeProperty`](#KeyframeProperty) in a actor's property track.
+   * Calculate the midpoint between this
+   * [`Rekapi.KeyframeProperty`](#KeyframeProperty) and the next
+   * [`Rekapi.KeyframeProperty`](#KeyframeProperty) in a actor's property
+   * track.
    *
-   * In just about all cases, `millisecond` should be between this [`Rekapi.KeyframeProperty`](#KeyframeProperty)'s `millisecond` and the `millisecond` of the [`Rekapi.KeyframeProperty`](#KeyframeProperty) that follows it in the animation timeline, but it is valid to specify a value outside of this range.
-   * @param {number} millisecond The millisecond in the animation timeline to compute the state value for.
+   * In just about all cases, `millisecond` should be between this
+   * [`Rekapi.KeyframeProperty`](#KeyframeProperty)'s `millisecond` and the
+   * `millisecond` of the [`Rekapi.KeyframeProperty`](#KeyframeProperty) that
+   * follows it in the animation timeline, but it is valid to specify a value
+   * outside of this range.
+   * @method getValueAt
+   * @param {number} millisecond The millisecond in the animation timeline to
+   * compute the state value for.
    * @return {number}
    */
   KeyframeProperty.prototype.getValueAt = function (millisecond) {
@@ -84,16 +104,26 @@ rekapiModules.push(function (context) {
   };
 
   /**
-   * Create the reference to the [`Rekapi.KeyframeProperty`](#KeyframeProperty) that follows this one on an actor's property track.  Property tracks are just linked lists of [`Rekapi.KeyframeProperty`](#KeyframeProperty)s.
-   * @param {Rekapi.KeyframeProperty} nextProperty The KeyframeProperty that should immediately follow this one on the animation timeline.
+   * Create the reference to the [`Rekapi.KeyframeProperty`](#KeyframeProperty)
+   * that follows this one on an actor's property track.  Property tracks are
+   * just linked lists of [`Rekapi.KeyframeProperty`](#KeyframeProperty)s.
+   * @method linkToNext
+   * @param {Rekapi.KeyframeProperty} nextProperty The KeyframeProperty that
+   * should immediately follow this one on the animation timeline.
    */
   KeyframeProperty.prototype.linkToNext = function (nextProperty) {
     this.nextProperty = nextProperty || null;
   };
 
   /**
-   * Disassociates this [`Rekapi.KeyframeProperty`](#KeyframeProperty) from its [`Rekapi.Actor`](rekapi.actor.js.html#Actor), if it has one.  This is called automatically by various [`Rekapi.Actor`](rekapi.actor.js.html#Actor) methods and triggers the [`removeKeyframeProperty`](rekapi.core.js.html#on) event on the associated [`Rekapi`](rekapi.core.js.html#Rekapi) instance, if there is one.
-   * @return {Rekapi.KeyframeProperty}
+   * Disassociates this [`Rekapi.KeyframeProperty`](#KeyframeProperty) from its
+   * [`Rekapi.Actor`](rekapi.actor.js.html#Actor), if it has one.  This is
+   * called automatically by various
+   * [`Rekapi.Actor`](rekapi.actor.js.html#Actor) methods and triggers the
+   * [`removeKeyframeProperty`](rekapi.core.js.html#on) event on the associated
+   * [`Rekapi`](rekapi.core.js.html#Rekapi) instance, if there is one.
+   * @method detach
+   * @chainable
    */
   KeyframeProperty.prototype.detach = function () {
     var actor = this.actor;
@@ -109,9 +139,11 @@ rekapiModules.push(function (context) {
   };
 
   /**
-   * Export a serializable Object of this [`Rekapi.KeyframeProperty`](#KeyframeProperty)'s state data.
+   * Export a serializable Object of this
+   * [`Rekapi.KeyframeProperty`](#KeyframeProperty)'s state data.
    *
    * __[Example](../../../../docs/examples/keyprop_export_property_data.html)__
+   * @method exportPropertyData
    * @return {Object}
    */
   KeyframeProperty.prototype.exportPropertyData = function () {
@@ -126,6 +158,7 @@ rekapiModules.push(function (context) {
   /*!
    * Whether or not this is a function keyframe and should be invoked for the
    * current frame.  Helper method for Rekapi.Actor.
+   * @method shouldInvokeForMillisecond
    * @return {boolean}
    */
   KeyframeProperty.prototype.shouldInvokeForMillisecond =
@@ -137,6 +170,7 @@ rekapiModules.push(function (context) {
 
   /**
    * Assuming this is a function keyframe, call the function.
+   * @method invoke
    * @return {*} Whatever value is returned from the bound function.
    */
   KeyframeProperty.prototype.invoke = function () {
